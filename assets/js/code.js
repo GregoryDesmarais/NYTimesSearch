2f0001c2b30920b89a191806ee6fec428a03193c
Scott Daniels <sdaniels6363@gmail.com>
2019-08-17 13:35:34 -0400

    added if/else and for loop for records check

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,5 +1,10 @@
-var urlBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="
+var urlBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=uZzKTkg7AVqi3Noss7jYB0x8haAR4dUd&q=";
 
+// pull values from the form after submit is hit.
+var searchTerm = $("#query").val();
+var recordNumber = $("#records").val();
+var startYr = $("#sYear").val();
+var endYr = $("#eYear").val();
 
 // This is the basic buildout of the API URL call
 if (searchTerm) {
@@ -9,6 +14,22 @@ if (searchTerm) {
 }
 
 if (recordNumber) {
+
+  // how many records were requested
+  if (recordNumber > 10){
+
+    var numberOfPages = Math.ceil( recordNumber / 10); // round up to the next whole number
+
+    for ( i = 0; i < numberOfPages; i++){
+
+
+
+      
+    }
+  }
+
+
+
   queryURL += "&" + recordNumber
 } else {
   alert("Field must be filled out");
